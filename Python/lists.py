88c031168445279eaf3ece85f8a35d575a242935
Jos Vlaar <github.com@josvlaar.com>
2022-09-22 12:29:28 +0200

    Update lists.py

--- Python/lists.py
+++ Python/lists.py
@@ -22,10 +22,6 @@ print(list9)
 print(list9[0])
 print(list9[1][3])
 
-list10 = [1,2,3]
-list10 -= [2]
-print(list10)
-
 # all the methods of list objects:
 x = 10
 y = [1,2,3]
@@ -37,8 +33,8 @@ y.remove(3)     # remove first matching element
 y.pop()         # remove and return last element
 y.pop(2)        # remove and return element at given index
 z.clear()       # remove all elements from list
-y.index(3)      # return index of first matching argument in the list
-y.index(3,0,3)  # limit search to subsequence of the list
+y.index(2)      # return index of first matching argument in the list
+y.index(2,0,3)  # limit search to subsequence of the list
 y.count(3)      # return number of times argument appears in list
 y.sort()        # sort list
 y.sort(key=None, reverse=False) # sort list in place
@@ -52,4 +48,32 @@ queue.append("Terry")           # Terry arrives
 queue.append("Graham")          # Graham arrives
 queue.popleft()                 # The first to arrive now leaves
 queue.popleft()                 # The second to arrive now leaves
-queue                           # Remaining queue in order of arrival
+print(queue)                    # Remaining queue in order of arrival
+
+
+squares = []
+for x in range(10):
+    squares.append(x**2)
+squares = list(map(lambda x: x**2, range(10)))
+squares = [x**2 for x in range(10)]
+print (squares)
+
+combinations = [(x,y) for x in [1,2,3] for y in [3,1,4] if x != y]
+combinations = []
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x != y:
+            combinations.append((x,y))
+
+vec = [-4,-2,0,2,4]
+res = [x*2 for x in vec]
+res = [x for x in vec if x >= 0]
+res = [abs(x) for x in vec]
+freshfruit = [' banana', ' loganberry', 'passion fruit ']
+res = [weapon.strip() for weapon in freshfruit]
+res = [(x, x**2) for x in range(6)]
+vec = [[1,2,3],[4,5,6],[7,8,9]]
+res = [num for elem in vec for num in elem]
+
+from math import pi
+res = [str(round(pi,n)) for n in range(1,6)]
